3d0bda0d950bce30302d6260a82320299ef4484c
Jeff Ndegwa <ndegwajeff4@gmail.com>
2024-04-03 06:52:52 +0300

    navbar & homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import boreka from "./assets/logo.png";
+import chevronleft from "./assets/chevron-down-svgrepo-com.png";
+import volunteers from "./assets/Untitled.jpeg";
+import arrow from "./assets/arrow-svgrepo-com.png";
+import nature from "./assets/nature-svgrepo-com.png";
+import user from "./assets/f48c6785744a603fadd4ae1e9339c1ed.jpg";
+import linkedin from "./assets/linkedin icon.png";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="main-page">
+      <div className="navbar">
+        <div className="left-nav">
+          <button>Home</button>
+          <button>Contribution</button>
+          <button>Our Mission</button>
+        </div>
+
+        <div className="center-nav">
+          <button>Kenya One</button>
+        </div>
+
+        <div className="right-nav">
+          <button>Problems</button>
+          <button>Projects</button>
+          <button className="contact">Contact Us</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="home-page">
+        <div className="left-home-page">
+          <div className="left-home-page-img">
+            <img src={boreka} alt="" />
+            <p>Boreka Group</p>
+          </div>
+          <div className="left-home-page-text">
+            Saving <img src={chevronleft} alt="" /> Nature & <br />
+            Fighting Climate <br />
+            Change Together.
+          </div>
+          <div className="left-home-page-form">
+            <form action="">
+              <input type="text" placeholder="Enter Your Email" />
+              <input type="button" value="Subscribe" />
+            </form>
+          </div>
+
+          <div className="left-home-page-members">
+            <p>Members</p>
+          </div>
+        </div>
+        <div className="right-home-page">
+          <div className="volunteers">
+            <div className="left-volunteers">
+              <img src={volunteers} alt="" />
+              <p>
+                We && Our <br />
+                Volunteers
+              </p>
+              <img src={arrow} alt="" />
+            </div>
+            <div className="right-volunteers">
+              <img src={nature} alt="" />
+            </div>
+          </div>
+          <div className="right-home-page-footer">
+            <div className="right-home-page-footer-one">
+              <img src={user} alt="" />
+              <p>
+                Join us in Fighting <br />
+                environmental problems
+              </p>
+            </div>
+
+            <div className="right-home-page-footer-two">
+              <p>
+                {" "}
+                <img src={linkedin} alt="" /> Find Us On Our Social Networks{" "}
+              </p>
+              <p>
+                We are an organization dedicated to <br />
+                protetcting the enviroment & nature.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
